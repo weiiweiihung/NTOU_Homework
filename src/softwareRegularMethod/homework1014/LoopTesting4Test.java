5ea869afad823129b15c16029f0ce729babec235
Jeffrey Hsu <jeffreyhsulife@gmail.com>
2023-10-21 16:24:01 +0800

    Add test 4 with test class to check isPositive

---
+++ src/softwareRegularMethod/homework1014/LoopTesting4Test.java
@@ -0,0 +1,24 @@
+package softwareRegularMethod.homework1014;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class LoopTesting4Test {
+
+	@Test
+	public void test1() {
+		LoopTesting4 test4 = new LoopTesting4();
+		for (int i = 0; i <= 10; i++) {
+			assertTrue(test4.isPositive(i));
+		}
+	}
+	
+	@Test
+	public void test() {
+		LoopTesting4 test4 = new LoopTesting4();
+		assertFalse(test4.isPositive(-1));
+	}
+}
