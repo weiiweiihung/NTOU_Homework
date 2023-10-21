43f8a88ca9295f8396f447d2d967e30a7758ae9b
Jeffrey Hsu <jeffreyhsulife@gmail.com>
2023-10-21 21:00:21 +0800

    One more condition to check if null or empty string list, make the coverage 100% without changing logic

--- src/softwareRegularMethod/homework1014/LoopTesting5_phone.java
+++ src/softwareRegularMethod/homework1014/LoopTesting5_phone.java
@@ -8,15 +8,23 @@ import java.util.ArrayList;
  *	
  */
 public class LoopTesting5_phone {
-	public static String checkPhone(ArrayList<String> strList){
+	public String checkPhone(ArrayList<String> strList){
+		if (strList == null || strList.size() <= 0)
+			return LoopTesting5_Common.ERR_NULL_OR_EMPTY.getContent();
+		
+		String msg = "";
+		
 		for(String str:strList){
-			if(str.length()!=10)
-				return LoopTesting5_Common.ERR_LENGTH.getContent();
+			if (str == null)
+				msg = LoopTesting5_Common.ERR_NULL_OR_EMPTY.getContent();
+			else if(str.length() != 10)
+				msg = LoopTesting5_Common.ERR_LENGTH.getContent();
 			else if(!str.matches("^[0-9]*$"))
-				return LoopTesting5_Common.ERR_FORMAT.getContent();
+				msg = LoopTesting5_Common.ERR_FORMAT.getContent();
 			else 
-				return LoopTesting5_Common.OK.getContent();
+				msg = LoopTesting5_Common.OK.getContent();
 		}
-		return "";
+		
+		return msg;
 	}
 }
