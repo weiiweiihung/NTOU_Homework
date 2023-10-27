eb1a675ff8ba5683ce99b29ad27e44cb11171066
沈君豪シンクンゴウ <aa032586@au.edu.tw>
2023-10-27 09:13:02 +0800

    增加行數註解

--- src/softwareRegularMethod/homework1014/LoopTesting5_phone.java
+++ src/softwareRegularMethod/homework1014/LoopTesting5_phone.java
@@ -6,22 +6,22 @@ package softwareRegularMethod.homework1014;
  *	
  */
 public class LoopTesting5_phone {
-	public String checkPhone(String[] strArrPhoneNumber){
-		if (strArrPhoneNumber == null || (strArrPhoneNumber.length == 1 && "".equals(strArrPhoneNumber[0])))
-			return LoopTesting5_Common.ERR_NULL.getContent();
-		if (strArrPhoneNumber.length != 10) 
-			return LoopTesting5_Common.ERR_LENGTH.getContent();
-		if (!"0".equals(strArrPhoneNumber[0]) && !"9".equals(strArrPhoneNumber[1])) {
-			return LoopTesting5_Common.ERR_FORMAT.getContent();
+	public String checkPhone(String[] strArrPhoneNumber){							      //1
+		if (strArrPhoneNumber == null || (strArrPhoneNumber.length == 1 && "".equals(strArrPhoneNumber[0])))  //2
+			return LoopTesting5_Common.ERR_NULL.getContent();					      //3
+		if (strArrPhoneNumber.length != 10) 							 	      //4
+			return LoopTesting5_Common.ERR_LENGTH.getContent();					      //5
+		if (!"0".equals(strArrPhoneNumber[0]) && !"9".equals(strArrPhoneNumber[1])) {			      //6
+			return LoopTesting5_Common.ERR_FORMAT.getContent();					      //7
 		}
 				
-		for(String str:strArrPhoneNumber){
-			if (" ".equals(str))
-				return LoopTesting5_Common.ERR_EMPTY.getContent();
-			else if(!str.matches("^[0-9]*$"))
-				return LoopTesting5_Common.ERR_FORMAT.getContent();
+		for(String str:strArrPhoneNumber){								      //8
+			if (" ".equals(str))									      //9
+				return LoopTesting5_Common.ERR_EMPTY.getContent();				      //10
+			else if(!str.matches("^[0-9]*$"))							      //11
+				return LoopTesting5_Common.ERR_FORMAT.getContent();				      //12
 		}
 		
-		return LoopTesting5_Common.OK.getContent();
+		return LoopTesting5_Common.OK.getContent();							      //13
 	}
 }
