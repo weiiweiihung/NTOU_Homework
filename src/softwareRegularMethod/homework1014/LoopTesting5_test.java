1697e5c40d3b5b99016a978e1816066824a503dc
Jenny <tuluber@gmail.com>
2023-10-27 09:29:30 +0800

    復原與新增

--- src/softwareRegularMethod/homework1014/LoopTesting5_test.java
+++ src/softwareRegularMethod/homework1014/LoopTesting5_test.java
@@ -1,6 +1,9 @@
 package softwareRegularMethod.homework1014;
 
 import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+
 import org.junit.Test;
 
 public class LoopTesting5_test {
@@ -8,60 +11,43 @@ public class LoopTesting5_test {
 	
 	@Test
 	public void test1() {
-		String [] strArrPhoneNumber = null;
-		String msg = new LoopTesting5_phone().checkPhone(strArrPhoneNumber);
+		ArrayList<String>strList = new ArrayList<>();
+		String msg = new LoopTesting5_phone().checkPhone(strList);
 		System.out.println(msg);
-		assertEquals(LoopTesting5_Common.ERR_NULL.getContent(), msg);
+		assertEquals(LoopTesting5_Common.ERR_NULL_OR_EMPTY.getContent(), msg);
 	}
 	@Test
 	public void test2() {
-		String strPhoneNumber = "1123";
-		String [] strArrPhoneNumber = strPhoneNumber.split("(?!^)");//轉array
-		String msg = new LoopTesting5_phone().checkPhone(strArrPhoneNumber);
+		
+		ArrayList<String>strList = new ArrayList<>();
+		strList.add("1123");
+		String msg = new LoopTesting5_phone().checkPhone(strList);
 		System.out.println(msg);
 		assertEquals(LoopTesting5_Common.ERR_LENGTH.getContent(), msg);
 	}
 	@Test
 	public void test3() {
-		String strPhoneNumber = "09123@@678";
-		String [] strArrPhoneNumber = strPhoneNumber.split("(?!^)");//轉array
-		String msg = new LoopTesting5_phone().checkPhone(strArrPhoneNumber);
+		ArrayList<String>strList = new ArrayList<>();
+		strList.add("@@!!@@!!@@");
+		String msg = new LoopTesting5_phone().checkPhone(strList);
 		System.out.println(msg);
 		assertEquals(LoopTesting5_Common.ERR_FORMAT.getContent(), msg);
 	}
 	@Test
 	public void test4() {
-		String strPhoneNumber = "0912345678";
-		String [] strArrPhoneNumber = strPhoneNumber.split("(?!^)");//轉array
-		String msg = new LoopTesting5_phone().checkPhone(strArrPhoneNumber);
+		ArrayList<String>strList = new ArrayList<>();
+		strList.add("0912345678");
+		String msg = new LoopTesting5_phone().checkPhone(strList);
 		System.out.println(msg);
 		assertEquals(LoopTesting5_Common.OK.getContent(), msg);
 	}
 	@Test
 	public void test5() {
-		String strPhoneNumber = "          ";
-		strPhoneNumber = strPhoneNumber.trim();
-		String [] strArrPhoneNumber = strPhoneNumber.split("(?!^)");//轉array
-		String msg = new LoopTesting5_phone().checkPhone(strArrPhoneNumber);
+		
+		ArrayList<String>strList = new ArrayList<>();
+		strList.add(null);
+		String msg = new LoopTesting5_phone().checkPhone(strList);
 		System.out.println(msg);
-		assertEquals(LoopTesting5_Common.ERR_NULL.getContent(), msg);
-	}
-	@Test
-	public void test6() {
-		String strPhoneNumber = "0912345 78";
-		strPhoneNumber = strPhoneNumber.trim();
-		String [] strArrPhoneNumber = strPhoneNumber.split("(?!^)");//轉array
-		String msg = new LoopTesting5_phone().checkPhone(strArrPhoneNumber);
-		System.out.println(msg);
-		assertEquals(LoopTesting5_Common.ERR_EMPTY.getContent(), msg);
-	}
-	@Test
-	public void test7() {
-		String strPhoneNumber = "1234567890";
-		strPhoneNumber = strPhoneNumber.trim();
-		String [] strArrPhoneNumber = strPhoneNumber.split("(?!^)");//轉array
-		String msg = new LoopTesting5_phone().checkPhone(strArrPhoneNumber);
-		System.out.println(msg);
-		assertEquals(LoopTesting5_Common.ERR_FORMAT.getContent(), msg);
+		assertEquals(LoopTesting5_Common.ERR_NULL_OR_EMPTY.getContent(), msg);
 	}
 }
