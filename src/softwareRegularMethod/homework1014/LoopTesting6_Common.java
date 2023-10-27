8cc3f4e9fc8531046a1101de8603d2e7fbc9c218
Jenny <tuluber@gmail.com>
2023-10-27 13:24:12 +0800

    修改物件名稱

--- src/softwareRegularMethod/homework1014/LoopTesting6_Common.java
+++ src/softwareRegularMethod/homework1014/LoopTesting6_Common.java
@@ -5,15 +5,16 @@ package softwareRegularMethod.homework1014;
  * @author Jenny
  *
  */
-public enum LoopTesting5_Common {
+public enum LoopTesting6_Common {
 	OK("輸入正確"),
 	ERR_LENGTH("長度錯誤"),
 	ERR_FORMAT("格式錯誤"),
+	ERR_EMPTY("輸入的號碼有空格"),
 	ERR_NULL("輸入的資料為空");
 	
     private final String content;
     
-    LoopTesting5_Common(String content) {
+    LoopTesting6_Common(String content) {
         this.content = content;
     }
     public String getContent() {
--- src/softwareRegularMethod/homework1014/LoopTesting6_test.java
+++ src/softwareRegularMethod/homework1014/LoopTesting6_test.java
@@ -3,39 +3,39 @@ package softwareRegularMethod.homework1014;
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
-public class LoopTesting5_test {
+public class LoopTesting6_test {
 	String message = "";
 	
 	@Test
 	public void test1() {
 		String [] strArrPhoneNumber = null;
-		String msg = new LoopTesting5_phone().checkPhone(strArrPhoneNumber);
+		String msg = new LoopTesting6_phone().checkPhone(strArrPhoneNumber);
 		System.out.println(msg);
-		assertEquals(LoopTesting5_Common.ERR_NULL.getContent(), msg);
+		assertEquals(LoopTesting6_Common.ERR_NULL.getContent(), msg);
 	}
 	@Test
 	public void test2() {
 		String strPhoneNumber = "1123";
 		String [] strArrPhoneNumber = strPhoneNumber.split("(?!^)");//轉array
-		String msg = new LoopTesting5_phone().checkPhone(strArrPhoneNumber);
+		String msg = new LoopTesting6_phone().checkPhone(strArrPhoneNumber);
 		System.out.println(msg);
-		assertEquals(LoopTesting5_Common.ERR_LENGTH.getContent(), msg);
+		assertEquals(LoopTesting6_Common.ERR_LENGTH.getContent(), msg);
 	}
 	@Test
 	public void test3() {
 		String strPhoneNumber = "09123@@678";
 		String [] strArrPhoneNumber = strPhoneNumber.split("(?!^)");//轉array
-		String msg = new LoopTesting5_phone().checkPhone(strArrPhoneNumber);
+		String msg = new LoopTesting6_phone().checkPhone(strArrPhoneNumber);
 		System.out.println(msg);
-		assertEquals(LoopTesting5_Common.ERR_FORMAT.getContent(), msg);
+		assertEquals(LoopTesting6_Common.ERR_FORMAT.getContent(), msg);
 	}
 	@Test
 	public void test4() {
 		String strPhoneNumber = "0912345678";
 		String [] strArrPhoneNumber = strPhoneNumber.split("(?!^)");//轉array
-		String msg = new LoopTesting5_phone().checkPhone(strArrPhoneNumber);
+		String msg = new LoopTesting6_phone().checkPhone(strArrPhoneNumber);
 		System.out.println(msg);
-		assertEquals(LoopTesting5_Common.OK.getContent(), msg);
+		assertEquals(LoopTesting6_Common.OK.getContent(), msg);
 	}
 	@Test
 	public void test5() {
@@ -43,26 +43,26 @@ public class LoopTesting5_test {
 		strPhoneNumber = strPhoneNumber.trim();
 		String [] strArrPhoneNumber = strPhoneNumber.split("(?!^)");//轉array
 		System.out.println(strArrPhoneNumber.length);
-		String msg = new LoopTesting5_phone().checkPhone(strArrPhoneNumber);
+		String msg = new LoopTesting6_phone().checkPhone(strArrPhoneNumber);
 		System.out.println(msg);
-		assertEquals(LoopTesting5_Common.ERR_NULL.getContent(), msg);
+		assertEquals(LoopTesting6_Common.ERR_NULL.getContent(), msg);
 	}
 	@Test
 	public void test6() {
 		String strPhoneNumber = "0912345 78";
 		strPhoneNumber = strPhoneNumber.trim();
 		String [] strArrPhoneNumber = strPhoneNumber.split("(?!^)");//轉array
-		String msg = new LoopTesting5_phone().checkPhone(strArrPhoneNumber);
+		String msg = new LoopTesting6_phone().checkPhone(strArrPhoneNumber);
 		System.out.println(msg);
-		assertEquals(LoopTesting5_Common.ERR_EMPTY.getContent(), msg);
+		assertEquals(LoopTesting6_Common.ERR_EMPTY.getContent(), msg);
 	}
 	@Test
 	public void test7() {
 		String strPhoneNumber = "1234567890";
 		strPhoneNumber = strPhoneNumber.trim();
 		String [] strArrPhoneNumber = strPhoneNumber.split("(?!^)");//轉array
-		String msg = new LoopTesting5_phone().checkPhone(strArrPhoneNumber);
+		String msg = new LoopTesting6_phone().checkPhone(strArrPhoneNumber);
 		System.out.println(msg);
-		assertEquals(LoopTesting5_Common.ERR_FORMAT.getContent(), msg);
+		assertEquals(LoopTesting6_Common.ERR_FORMAT.getContent(), msg);
 	}
 }
