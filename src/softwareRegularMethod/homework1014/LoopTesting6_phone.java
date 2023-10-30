6db83d03c2b4855ff500a04d58a3b7d208f4634e
Jenny <tuluber@gmail.com>
2023-10-30 08:53:32 +0800

    修正:幾個return幾個test,開頭09使用正則

--- src/softwareRegularMethod/homework1014/LoopTesting6_phone.java
+++ src/softwareRegularMethod/homework1014/LoopTesting6_phone.java
@@ -1,26 +1,30 @@
 package softwareRegularMethod.homework1014;
-
-/**
- * check phone number
- * @author Jenny
- *	
- */
-public class LoopTesting6_phone {
-	public String checkPhone(String[] strArrPhoneNumber){							        //1
-		if (strArrPhoneNumber == null)    //2
-			return LoopTesting6_Common.ERR_NULL.getContent();						//3
-		if (strArrPhoneNumber.length != 10) 									//4
-			return LoopTesting6_Common.ERR_LENGTH.getContent();						//5
-		if (!"0".equals(strArrPhoneNumber[0]) && !"9".equals(strArrPhoneNumber[1])||!"0".equals(strArrPhoneNumber[0])||!"9".equals(strArrPhoneNumber[1])) {//6
-			return LoopTesting6_Common.ERR_FORMAT.getContent();						//7
-		}
-				
-		for(String str:strArrPhoneNumber){									//8
-			if(!str.matches("^[0-9]*$"))									//9
-				return LoopTesting6_Common.ERR_FORMAT.getContent();					//10
-		}
-		
-		return LoopTesting6_Common.OK.getContent();								//11
-	}
-}
-
+																		    
+/**                                                                         
+ * check phone number                                                       
+ * @author Jenny                                                            
+ *	                                                                        
+ */                                                                         
+public class LoopTesting6_phone {                                           
+	public String checkPhone(String[] strArrPhoneNumber){				// 1	 
+		if (strArrPhoneNumber == null)                                  // 2
+			return LoopTesting6_Common.ERR_NULL.getContent();			// 3
+		if (strArrPhoneNumber.length != 10) 							// 4 
+			return LoopTesting6_Common.ERR_LENGTH.getContent();			// 5
+		int index = 0;		                                            // 6  
+		for(String str:strArrPhoneNumber){                              // 7
+			if(index==0)                                                // 8  
+				if(!str.matches("[0]")) 								// 9
+					return LoopTesting6_Common.ERR_FORMAT.getContent();	// 10
+			if(index==1)                                                // 11
+				if(!str.matches("[9]")) 								// 12
+					return LoopTesting6_Common.ERR_FORMAT.getContent();	// 13
+			if(index>=2) {                                              // 14
+				if(!str.matches("^[0-9]*$")) 							// 15
+					return LoopTesting6_Common.ERR_FORMAT.getContent();	// 16
+			}                                                            
+			index++;                                                    // 17
+		}                                                               
+		return LoopTesting6_Common.OK.getContent();						// 18
+	}                                                                       
+}                                                                           
--- src/softwareRegularMethod/homework1014/LoopTesting6_test.java
+++ src/softwareRegularMethod/homework1014/LoopTesting6_test.java
@@ -22,44 +22,36 @@ public class LoopTesting6_test {
 		assertEquals(LoopTesting6_Common.ERR_LENGTH.getContent(), msg);
 	}
 	@Test
-	public void test3() {
-		String strPhoneNumber = "09123@@678";
-		String [] strArrPhoneNumber = strPhoneNumber.split("(?!^)");//轉array
-		String msg = new LoopTesting6_phone().checkPhone(strArrPhoneNumber);
-		System.out.println(msg);
-		assertEquals(LoopTesting6_Common.ERR_FORMAT.getContent(), msg);
-	}
-	@Test
-	public void test4() {
-		String strPhoneNumber = "0912345678";
-		String [] strArrPhoneNumber = strPhoneNumber.split("(?!^)");//轉array
-		String msg = new LoopTesting6_phone().checkPhone(strArrPhoneNumber);
-		System.out.println(msg);
-		assertEquals(LoopTesting6_Common.OK.getContent(), msg);
-	}
-	@Test
-	public void test5() {
+	public void test3() {// 第1碼不為0
 		String strPhoneNumber = "1234567890";
 		strPhoneNumber = strPhoneNumber.trim();
 		String [] strArrPhoneNumber = strPhoneNumber.split("(?!^)");//轉array
 		String msg = new LoopTesting6_phone().checkPhone(strArrPhoneNumber);
 		System.out.println(msg);
 		assertEquals(LoopTesting6_Common.ERR_FORMAT.getContent(), msg);
-	}	
+	}
 	@Test
-	public void test6() {
-		String strPhoneNumber = "1912345678";
+	public void test4() {// 第2碼不為9
+		String strPhoneNumber = "0112345678";
 		String [] strArrPhoneNumber = strPhoneNumber.split("(?!^)");//轉array
 		String msg = new LoopTesting6_phone().checkPhone(strArrPhoneNumber);
 		System.out.println(msg);
 		assertEquals(LoopTesting6_Common.ERR_FORMAT.getContent(), msg);
 	}
 	@Test
-	public void test7() {
-		String strPhoneNumber = "0112345678";
+	public void test5() {
+		String strPhoneNumber = "09@@123456";
 		String [] strArrPhoneNumber = strPhoneNumber.split("(?!^)");//轉array
 		String msg = new LoopTesting6_phone().checkPhone(strArrPhoneNumber);
 		System.out.println(msg);
 		assertEquals(LoopTesting6_Common.ERR_FORMAT.getContent(), msg);
+	}
+	@Test
+	public void test6() {//OK
+		String strPhoneNumber = "0912345678";
+		String [] strArrPhoneNumber = strPhoneNumber.split("(?!^)");//轉array
+		String msg = new LoopTesting6_phone().checkPhone(strArrPhoneNumber);
+		System.out.println(msg);
+		assertEquals(LoopTesting6_Common.OK.getContent(), msg);
 	}
 }
