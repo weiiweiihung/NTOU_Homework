adcd9ba87229688628501a59782d7ce8f7257f29
沈君豪シンクンゴウ <aa032586@au.edu.tw>
2023-12-04 11:22:31 +0800

    Update HW7_LoopTesting_test.java
    新增測試案例

--- src/softwareRegularMethod/homework1209/HW7_LoopTesting_test.java
+++ src/softwareRegularMethod/homework1209/HW7_LoopTesting_test.java
@@ -7,14 +7,42 @@ import softwareRegularMethod.homework1118.HW4_LoopTesting_Common;
 public class HW7_LoopTesting_test {
 	/*
 	 * 第7次作業
-	 * 正確一次，各種錯誤各一次
+	 * 正確一次，各種錯誤各一次(四種錯誤)
+  	 * 總共五次
 	 */
 	@Test
 	public void testMethod1ForP() {
-	    // 在這裡執行測試程式碼
-		String strPhoneNumber = "09123";//長度錯誤or第一、二碼不為09，(判斷是否皆為數字邏輯上走不到，因為前面就會直接RETURN)True
+		String strPhoneNumber = "0912345678";//正確
+		String msg = new HW7_LoopTesting_phone().checkPhone(strPhoneNumber);
+		assertEquals(HW4_LoopTesting_Common.OK.getContent(), msg);   
+	}
+
+	@Test
+	public void testMethod2ForP() {
+		String strPhoneNumber = "123456";//長度非為10
 		String msg = new HW7_LoopTesting_phone().checkPhone(strPhoneNumber);
 		assertEquals(HW4_LoopTesting_Common.ERR_FORMAT.getContent(), msg);   
+	}
+
+	@Test
+	public void testMethod3ForP() {
+		String strPhoneNumber = "9912345678";//第一碼不是0
+		String msg = new HW7_LoopTesting_phone().checkPhone(strPhoneNumber);
+		assertEquals(HW4_LoopTesting_Common.ERR_FORMAT.getContent(), msg);   
+	}
+
+	@Test
+	public void testMethod4ForP() {
+		String strPhoneNumber = "0012345678";//第二碼不是9
+		String msg = new HW7_LoopTesting_phone().checkPhone(strPhoneNumber);
+		assertEquals(HW4_LoopTesting_Common.ERR_FORMAT.getContent(), msg);   
+	}
+
+	@Test
+	public void testMethod5ForP() {
+		String strPhoneNumber = "09%$#45678";//包含非數字字元
+		String msg = new HW7_LoopTesting_phone().checkPhone(strPhoneNumber);
+		assertEquals(HW4_LoopTesting_Common.ERR_NUMBER.getContent(), msg);   
 	}
 	
 		
