2a9c00be8c37df3637aefe63ecc3e7dbb438f331
沈君豪シンクンゴウ <aa032586@au.edu.tw>
2023-12-07 13:59:35 +0800

    Update HW8_TestingScoreTest.java

--- src/softwareRegularMethod/homework1209_HW2/HW8_TestingScoreTest.java
+++ src/softwareRegularMethod/homework1209_HW2/HW8_TestingScoreTest.java
@@ -10,5 +10,35 @@ public class HW8_TestingScoreTest {
 		assertEquals("合格", result);
 		System.out.println("test1 response: " + result);
 	}
+
+	//邊界值 50,60,70
+	@Test
+	public void test2() {
+		String result = new HW8_TestingScore().getAvgScore(new int[] { 50, 80 });
+		assertEquals("不合格", result);
+		System.out.println("test1 response: " + result);
+	}
+
+	@Test
+	public void test3() {
+		String result = new HW8_TestingScore().getAvgScore(new int[] { 70, 80 });
+		assertEquals("合格", result);
+		System.out.println("test1 response: " + result);
+	}
+
+	//邊界值 70,80,90
+	@Test
+	public void test4() {
+		String result = new HW8_TestingScore().getAvgScore(new int[] { 60, 70 });
+		assertEquals("不合格", result);
+		System.out.println("test1 response: " + result);
+	}
+
+	@Test
+	public void test5() {
+		String result = new HW8_TestingScore().getAvgScore(new int[] { 60, 90 });
+		assertEquals("合格", result);
+		System.out.println("test1 response: " + result);
+	}
 	
 }
