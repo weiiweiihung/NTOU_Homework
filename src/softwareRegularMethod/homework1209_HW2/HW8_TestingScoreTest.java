a5f7da79a5ade4f8e82cd8ed411e6d3bac2325de
沈君豪シンクンゴウ <aa032586@au.edu.tw>
2023-12-07 14:35:39 +0800

    Update HW8_TestingScoreTest.java

--- src/softwareRegularMethod/homework1209_HW2/HW8_TestingScoreTest.java
+++ src/softwareRegularMethod/homework1209_HW2/HW8_TestingScoreTest.java
@@ -11,32 +11,32 @@ public class HW8_TestingScoreTest {
 		System.out.println("test1 response: " + result);
 	}
 
-	//邊界值 50,60,70
+	//邊界值 59,60,61
 	@Test
 	public void test2() {
-		String result = new HW8_TestingScore().getAvgScore(new int[] { 50, 80 });
+		String result = new HW8_TestingScore().getAvgScore(new int[] { 59, 80 });
 		assertEquals("不合格", result);
 		System.out.println("test1 response: " + result);
 	}
 
 	@Test
 	public void test3() {
-		String result = new HW8_TestingScore().getAvgScore(new int[] { 70, 80 });
+		String result = new HW8_TestingScore().getAvgScore(new int[] { 61, 80 });
 		assertEquals("合格", result);
 		System.out.println("test1 response: " + result);
 	}
 
-	//邊界值 70,80,90
+	//邊界值 79,80,81
 	@Test
 	public void test4() {
-		String result = new HW8_TestingScore().getAvgScore(new int[] { 60, 70 });
+		String result = new HW8_TestingScore().getAvgScore(new int[] { 60, 79 });
 		assertEquals("不合格", result);
 		System.out.println("test1 response: " + result);
 	}
 
 	@Test
 	public void test5() {
-		String result = new HW8_TestingScore().getAvgScore(new int[] { 60, 90 });
+		String result = new HW8_TestingScore().getAvgScore(new int[] { 60, 81 });
 		assertEquals("合格", result);
 		System.out.println("test1 response: " + result);
 	}
